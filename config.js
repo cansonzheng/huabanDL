988fef66579e8b32bc0e086ba673cc0fe7d06881
Canson <z@zcc.ren>
2019-01-29 10:57:14 +0800

    修改配置文件说明

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 module.exports={
-
-  // 账号 填写到双引号之间
-  account:"", 
-
-  // 密码 填写到双引号之间
-  pwd:"", 
-
-  /* 指定采集的画板地址 默认全部画板
-  如果需要指定画板，填写示例 ["http://login.meiwu.co/boards/00000/","http://login.meiwu.co/boards/11111","http://login.meiwu.co/boards/22222"]
-  */
+  account:"替换为你的账号名", 
+  pwd:"替换为你的密码", 
   board:[]
 }
+
+/* 
+指定采集的画板地址 默认全部画板
+如果需要指定画板，填写示例 ["http://login.meiwu.co/boards/00000/","http://login.meiwu.co/boards/11111","http://login.meiwu.co/boards/22222"]
+*/
